5f3c25a4bd7122a5182d8930fbf3c0df340a8f45
Robby2012 <robby.upm@gmail.com>
2013-01-23 18:18:36 +0100

    weeVideo version 1.0

---
+++ static/script.js
@@ -0,0 +1,206 @@
+
+wz.app.addScript( 4, 'common', function( win, params ){
+	
+	var video = $('video',win);
+	var weevideoTop = $('.weevideo-top',win);
+	var weevideoBottom = $('.weevideo-bottom-shadow',win);
+	var weevideoCurrentTime = $('.currentTime',win);
+	var weevideoTotalTime = $('.totalTime',win);
+	var weevideoProgress = $('.weevideo-info-progress',win);
+	var weevideoBackprogress = $('.weevideo-info-backprogress',win);
+	var weevideoBufferprogress = $('.weevideo-info-buffer',win);
+	var weevideoSeeker = $('.weevideo-info-seeker',win);
+	var weevideoTitle = $('.weevideo-title-text',win);
+	var weevideoSeekerWidth = weevideoSeeker.width()/2;
+	
+	if( params && params.length ){
+		
+		wz.structure( params[0], function( error, structure ){
+			
+			video.append( $('<source></source>').attr('type','video/webm').attr('src', structure.formats.webm.url) );
+			video.append( $('<source></source>').attr('type','video/mp4').attr('src', structure.formats.mp4.url) );
+			weevideoTitle.text(structure.name);
+			
+		});
+		
+	}
+	
+	$( win )
+	
+		.on('click', '.weevideo-controls-play', function(){
+			
+			if( win.hasClass('play') ){
+				video[0].pause();
+			}else{
+				video[0].play();
+			}
+			
+		})
+		
+		.on('click', '.weevideo-volume-icon', function(){
+			
+			if( win.hasClass('muted') ){
+				video[0].muted = false;
+			}else{
+				video[0].muted = true;
+			}
+			
+		})
+		
+		.on('click', '.wz-win-maximize', function(){
+			
+			if( win.hasClass('maximized') ){
+				
+				if( video[0].cancelFullScreen ){ video[0].cancelFullScreen(); }
+				if( video[0].webkitCancelFullScreen ){ video[0].webkitCancelFullScreen(); }
+				if(	video[0].mozCancelFullScreen ){ video[0].mozCancelFullScreen(); }		
+				
+			}else{
+				
+				if( video[0].requestFullScreen ){ video[0].requestFullScreen(); }
+				if( video[0].webkitRequestFullScreen ){ video[0].webkitRequestFullScreen(); }
+				if(	video[0].mozRequestFullScreen ){ video[0].mozRequestFullScreen(); }
+				
+			}
+			
+		})
+		
+		.on('mouseleave', function(){
+	
+			weevideoTop.stop(true).animate({top:-172},1000);
+			weevideoBottom.stop(true).animate({bottom:-138},1000);
+
+		})
+
+		.on('mouseenter', function(){
+
+			weevideoTop.stop(true).animate({top:0},500);
+			weevideoBottom.stop(true).animate({bottom:0},500);
+			
+		})
+		
+		.on('click', '.weevideo-controls-rewind', function(){
+			
+			video[0].currentTime -= 10;
+			
+		})
+		
+		.on('click', '.weevideo-controls-forward', function(){
+			
+			video[0].currentTime += 10;
+			
+		});
+	
+	video
+	
+		.on('play',function(){
+			win.addClass('play');
+		})
+		
+		.on('pause',function(){
+			win.removeClass('play');
+		})
+		
+		.on('volumechange', function(){
+			
+			if( this.muted ){
+				win.addClass('muted');
+			}else{
+				win.removeClass('muted');
+			}
+			
+		})
+		
+		.on('fullscreenchange mozfullscreenchange webkitfullscreenchange', function(){
+			
+			if( this.fullScreen ){
+				win.addClass('fullscreen');
+			}else{
+				win.removeClass('fullscreen');
+			}
+			
+		})
+		
+		.on('timeupdate', function(e){
+						
+			var time = this.currentTime;
+			var hour = parseInt(time/3600);
+			var rem	 = (time%3600);
+			var min	 = parseInt(rem/60);
+			var sec	 = parseInt(rem%60);
+			
+			if(hour<10){ hour = '0'+hour}	
+			if(min <10){ min  = '0'+min }
+			if(sec <10){ sec  = '0'+sec }
+						
+			if(parseInt(hour)){
+				weevideoCurrentTime.text(hour+':'+min+':'+sec);
+			}else{
+				weevideoCurrentTime.text(min+':'+sec);
+			}
+			
+			var pos = weevideoBackprogress.width()*(this.currentTime/this.duration);
+
+			weevideoProgress.width(pos);
+
+			weevideoSeeker.css('left',pos-weevideoSeekerWidth);
+			
+		})
+		
+		.on('durationchange', function(e){
+												
+			var time = this.duration;
+			var hour = parseInt(time/3600);
+			var rem	 = (time%3600);
+			var min	 = parseInt(rem/60);
+			var sec	 = parseInt(rem%60);
+		
+			if(hour<10){ hour = '0'+hour}	
+			if(min <10){ min  = '0'+min }
+			if(sec <10){ sec  = '0'+sec }
+			
+			weevideoBackprogress.animate({'opacity':'1'},250);
+			
+			if(parseInt(hour)){
+				weevideoCurrentTime.animate({'opacity':'1'},250).text('00:00:00');
+				weevideoTotalTime.animate({'opacity':'1'},250).text(hour+':'+min+':'+sec);
+			}else{
+				weevideoCurrentTime.animate({'opacity':'1'},250).text('00:00');
+				weevideoTotalTime.animate({'opacity':'1'},250).text(min+':'+sec);
+			}
+		
+		})
+		
+		.on('progress',function(){
+						
+			try{
+				var buffer	= this.buffered.end(0);
+			}catch(e){}
+			
+			var width = (weevideoBackprogress.width()*(buffer/this.duration));
+			
+			if(width > 0){
+				weevideoBufferprogress.animate({width:width},100);
+			}
+					
+		})
+		
+		.on('ended', function(){
+						
+			var time = this.duration;
+			var hour = parseInt(time/3600);
+			weevideoProgress.width(0);
+			weevideoSeeker.css('left',9);
+			
+			if(parseInt(hour)){
+				weevideoCurrentTime.text('00:00:00');
+			}else{
+				weevideoCurrentTime.text('00:00');
+			}
+			
+			this.currentTime = 0;
+			this.pause();
+						
+		});
+	
+});
---
+++ static/script_antiguo.js
@@ -0,0 +1,113 @@
+$('.wz-app-6')
+
+.on('wz-new-window',function(){
+	
+	var video			= $('video',this);
+	var videoAppArea	= $(this).children('.wz-app-area');
+	var videoControls	= videoAppArea.children('.weevideo-controls');
+	var videoCurrent	= videoControls.children('.weevideo-current');
+	var videoTotal		= videoControls.children('.weevideo-total');
+	
+	var videoBackprogress	= videoControls.children('.weevideo-backprogress');
+	var videoBufferprogress	= videoBackprogress.children('.weevideo-bufferprogress');
+	var videoSeeker			= videoBackprogress.children('.weevideo-seeker');
+	var videoProgress		= videoBackprogress.children('.weevideo-progress');
+		
+	video
+
+		.on('volumechange', function(e){
+						
+			if(this.muted && videoAppArea.hasClass('muted')){
+				this.muted = false;
+				return false;
+			}
+			
+			if(this.muted){
+								
+				videoAppArea.addClass('muted').removeClass('volume-0 volume-33 volume-66 volume-100');
+				
+			}else{
+				
+				if(this.volume == 0){
+					videoAppArea.addClass('volume-0').removeClass('muted volume-33 volume-66 volume-100');
+				}else if(this.volume <= 0.33){
+					videoAppArea.addClass('volume-33').removeClass('muted volume-0 volume-66 volume-100');
+				}else if(this.volume <= 0.66){
+					videoAppArea.addClass('volume-66').removeClass('muted volume-0 volume-33 volume-100');
+				}else{
+					videoAppArea.addClass('volume-100').removeClass('muted volume-0 volume-33 volume-66');
+				}
+				
+			}
+			
+		})		
+		
+})
+
+.on('wz-dragend', '.weevideo-seeker', function(){
+	
+	var parent	= $(this).parents('.wz-app-area');
+	var app		= parent.parents('.wz-app');
+	
+	parent.children('video')[0].play();
+		
+	if(!app.is(':hover')){
+		app.mouseleave();
+	}
+	
+})
+
+.on('wz-dragend', '.weevideo-volume-seeker', function(){
+	
+	var app		= $(this).parents('.wz-app');
+			
+	if(!app.is(':hover')){
+		app.mouseleave();
+	}
+	
+})
+
+.on('wz-dragend', '.weevideo-volume-seeker', function(){
+	
+	var panel = $(this).parents('.weevideo-volume-panel');
+	
+	if(!panel.is(':hover')){
+		panel.mouseleave();
+	}
+	
+})
+
+.on('wz-dragmove', '.weevideo-seeker', function(e,drag,pos,per){
+	
+	var parent		= $(this).parent();
+	var video		= parent.parents('.wz-app-area').children('video');
+	
+	video[0].pause();
+	
+	var parentWidth = parent.width();
+	var seekerWidth = $(this).width();
+		
+	// Yep, this prevent the show timeupdate lag in the progress bar
+	parent.children('.weevideo-progress').width((parentWidth*per.x) + ((1-per.x)*seekerWidth));
+	
+	var time = per.x*video[0].duration;
+	
+	video[0].currentTime = time;
+	
+})
+
+.on('wz-dragmove', '.weevideo-volume-seeker', function(e,drag,pos,per){
+	
+	var parent		= $(this).parent();
+	var video		= parent.parents('.wz-app-area').children('video');
+	
+	var parentHeight = parent.height();
+	var seekerHeight = $(this).height();
+	
+	var height = (parentHeight*(1-per.y));
+	
+	parent.children('.weevideo-volume').css('top',parentHeight-height).height(height);
+	
+	video[0].volume = 1-per.y;
+	
+});
