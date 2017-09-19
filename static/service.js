a5eb66d0733ee1d174238c1632f68bee0f0d2007
Carlos Navas <carlosnavas1991@gmail.com>
2017-09-19 15:24:18 +0200

    Added service

---
+++ static/service.js
@@ -0,0 +1,69 @@
+var myUserID  = api.system.user().id;
+
+api.channel.on( 'userAdded' , function( channel , userAdded ){
+
+  api.channel( channel.id , function( e , channel ){
+
+      if (api.app.getViews().length != 0) {
+        api.app.getViews( 'main' ).trigger( 'getChats' , { id: channel.id } );
+      }
+
+  });
+
+});
+
+api.channel.on( 'userRemoved' , function( channel , userRemoved ){
+
+
+
+});
+
+api.channel.on( 'message' , function( info , o ){
+
+  // The app is oppened, so don't show the banner
+  if (api.app.getViews().length != 0) {
+    return;
+  }
+
+  // I am the sender , so don't show the banner
+  if (info.sender == myUserID) {
+    return;
+  }
+
+  // If recieved is a message increment Badge, and show the banner
+  if ( o.action === 'message' ) {
+
+    /* COMPRUEBO QUE NO ES UN MENSAJE REPETIDO, YA QUE NO SE PORQUE SE ENVIA 2 VECES AL HACER UN UNICO .send() */
+    if ( lastMessageReceived && o.id === lastMessageReceived.id ) {
+      return;
+    }
+    lastMessageReceived = o;
+    /* -- */
+
+    updateBadge( 1 , true );
+
+    api.user( info.sender, function( error, user ){
+
+      if( error ){
+        return;
+      }
+
+      var name = o.groupName ? o.groupName : user.fullName;
+
+      api.banner()
+        .setTitle( name )
+        .setText( o.txt )
+        .setIcon( user.avatar.tiny )
+        .on( 'click', function(){
+
+          //TODO funcion para sacar el id de mi app?
+          api.app.openApp( 232 , info.id , function(o){});
+
+        })
+        .render();
+
+    });
+
+  }
+
+});
